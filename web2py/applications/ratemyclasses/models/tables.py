090b803674e0e1642360a345e202c063b31385aa
Saiya Youn <sayoun@ucsc.edu>
2018-05-17 18:29:40 -0700

    Updated tables.py. Edited the myclass and reviews table

--- web2py/applications/ratemyclasses/models/tables.py
+++ web2py/applications/ratemyclasses/models/tables.py
@@ -19,39 +19,34 @@ def make_random_id():
     return randint(4,9000)
 
 
-db.define_table('myclass',
-                Field('course_name'),   #Full course name ex. Web Applications
-                Field('department', readable=False, writable=False),    #Can be full department name(i.e. Computer Science) or shorthand (i.e CMPS)
-                Field('course_num', readable=False, writable=False),    #Course number ex. 101
-                #Field('university'),    #To know which university this class belongs to
-                Field('genEd', readable=False, writable=False),         #General education requirement it satisfies
-                Field('info', readable=False, writable=False),          #The course details go here(a brief synopsis)
-                Field('bookmark', 'boolean', default=False, readable=False, writable=False), #If the student wants to bookmark it it's true else false
-                #Field('professor_name'), not sure how we want professor to be(input by the student or pre-approved, etc.)
-                Field('updated_on', 'datetime', update=datetime.datetime.utcnow(), readable=False, writable=False),
-                Field('school_id', readable=True, writable=False)
-)
-
-db.define_table('university',
-                Field('name'),          #Name of the university
-                Field('state_name'),    #State that the university is in
-                Field('city_name'),     #City the university is in
-                Field('zipcode'),       #Zipcode
-                Field('uni_bookmark', 'boolean', default=False) #A way to keep track if it's the student's school
-)
-
-db.define_table('reviews',
-                Field('overall_rate'),
-                Field('difficulty_rate'),
-                Field('rec_professor')
-)
-
 #Dummy table testing
 db.define_table('school',
                 Field('name', default=get_user_school()),
                 Field('school_id', default=make_random_id(), readable=False,    writable=False)
                 )
 
+db.define_table('myclass',
+                Field('course_name'),   #Full course name ex. Web Applications
+                Field('department', readable=False, writable=False),    #Can be full department name(i.e. Computer Science) or shorthand (i.e CMPS)
+                Field('course_num', readable=False, writable=False),    #Course number ex. 101
+                Field('genEd', readable=False, writable=False),         #General education requirement it satisfies
+                Field('info', readable=False, writable=False),          #The course details go here(a brief synopsis)
+                Field('bookmark', 'boolean', default=False, readable=False, writable=False), #If the student wants to bookmark it it's true else false
+                Field('updated_on', 'datetime', update=datetime.datetime.utcnow(), readable=False, writable=False),
+                Field('school_id', readable=True, writable=False),
+                Field('class_id', default=make_random_id(), readable=False, writable=False)
+)
+
+db.define_table('reviews',
+                #Field('class_id', 'reference myclass'),
+                Field('class_id', readable=True, writable=False),
+                Field('overall_rate'),
+                Field('difficulty_rate'),
+                Field('rec_professor'),
+                Field('grade'),
+                Field('teacher'),
+                Field('main_review', 'text')
+)
 
 # Pre-populate db with schools if none
 if db(db.school.id>0).count() == 0:
